c7b8ed5e1dd1476d51a520c932b1bbde6ec2792c
Wilson Laboy <wil.j.laboy@gmail.com>
2022-06-28 15:55:13 -0400

    Added Prizes logic

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,13 +1,9 @@
 import React, { useContext, useState } from "react";
+import { GameContext } from "./GameContext";
 import Team from "./Team";
 
 function Scoreboard() {
-  const Prizes = {
-    FirstPrize: 100,
-    SecondPrize: 75,
-    ThirdPrize: 50,
-    FourthPrize: 25,
-  };
+  const game = useContext(GameContext);
 
   const [teams, setTeams] = useState([
     { id: 1, name: "Team #1", score: 0 },
@@ -19,6 +15,7 @@ function Scoreboard() {
       { id: 1, name: "Team #1", score: 0 },
       { id: 2, name: "Team #2", score: 0 },
     ]);
+    game.handsPlayed = 0;
   };
 
   const updateScore = (id, newScore) => {
@@ -26,11 +23,49 @@ function Scoreboard() {
     setTeams(
       teams.map((team) => {
         if (team.id === teamToUpdate.id) {
-          return { ...team, score: team.score + newScore };
+          return {
+            ...team,
+            score: team.score + calculateNewScoreWithPrizes(newScore),
+          };
         }
         return team;
       })
     );
+    updateHandsPlayed();
+  };
+
+  const updateHandsPlayed = () => {
+    game.handsPlayed += 1;
+  };
+
+  const calculateNewScoreWithPrizes = (newScore) => {
+    const prizes = {
+      firstPrize: 100,
+      secondPrize: 75,
+      thirdPrize: 50,
+      fourthPrize: 25,
+    };
+
+    if (game.usePrizes) {
+      switch (game.handsPlayed) {
+        case 0:
+          newScore += prizes.firstPrize;
+          break;
+        case 1:
+          newScore += prizes.secondPrize;
+          break;
+        case 2:
+          newScore += prizes.thirdPrize;
+          break;
+        case 3:
+          newScore += prizes.fourthPrize;
+          break;
+        default:
+          newScore += 0;
+          break;
+      }
+    }
+    return newScore;
   };
 
   return (
